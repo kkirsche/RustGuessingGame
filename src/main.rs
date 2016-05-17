2c92596329b9a17777294e95de338686b79c0a87
Kevin Kirsche <kev.kirsche@gmail.com>
2016-05-17 16:33:22 -0400

    Run cargo-fmt

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ fn main() {
 
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
@@ -25,9 +26,9 @@ fn main() {
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less      => println!("Too small!"),
-            Ordering::Greater   => println!("Too big!"),
-            Ordering::Equal     => {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
                 println!("You win!");
                 break;
             }
